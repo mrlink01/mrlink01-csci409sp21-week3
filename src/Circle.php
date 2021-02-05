d84f18a3b6a711f232cfa7ab4ff80585dc18b3c7
Michael Link <mrlink@coastal.edu>
2021-02-05 13:52:04 -0500

    Created Circle Class

---
+++ src/Circle.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ *  Class:   csci409sp21
+ *  User:    mrlink
+ *  Date:    2/5/21
+ *  Time:    1:46 PM
+ */
+require_once "Shape.php";
+
+class Circle extends Shape {
+
+    const SHAPE_TYPE = 3;
+
+    protected $radius;
+
+    function __construct($radius) {
+        $this->radius = $radius;
+        parent::__construct(0, 0);
+    }
+
+    public function area() {
+        return pi() * pow($this->radius, 2);
+    }
+
+    public function getFullDescription() {
+        return "Circle<#" . $this->getId() . ">: " . $this->name .  " – " . $this->radius;
+    }
+
+}
+
+$foo = new Circle(3);
+
+$foo->setName("Circle1");
+echo $foo->getFullDescription();
